d9b1fa7c125e6cfcc7bc8f2f3152014817b2dee3
Ambareeshvs <ambareeshvs@gmail.com>
2023-03-24 11:25:14 +0530

    Front End commits

--- Frontend/Frontend/Controllers/AdministrativeController.cs
+++ Frontend/Frontend/Controllers/AdministrativeController.cs
@@ -9,131 +9,139 @@ using NToastNotify;
 
 namespace Frontend.Controllers
 {
-    public class AdministrativeController : BaseController
-    {
-        private readonly IGenericHttpClient _client;
-        private readonly IToastNotification _toastService;
+	public class AdministrativeController : BaseController
+	{
+		private readonly IGenericHttpClient _client;
+		private readonly IToastNotification _toastService;
 
-        public AdministrativeController(IGenericHttpClient client,
-            IToastNotification toastService):base(toastService)
-        {
-            _client = client;
-            _toastService = toastService;
-        }
+		public AdministrativeController(IGenericHttpClient client,
+			IToastNotification toastService) : base(toastService)
+		{
+			_client = client;
+			_toastService = toastService;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> RolesList()
-        {
-            try
-            {
-                var result = await _client.GetAllAsync<IdentityRole>(ApiConstants.GetAllRoles);
-                return View(result);
-            }
-            catch (Exception ex)
-            {
-                DisplayErrors(ex);
-                return View();
-            }
+		[HttpGet]
+		public IActionResult Index()
+		{
 
-        }
+			return View();
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> SingleRole(string roleId)
-        {
-            try
-            {
-                var result = await _client.GetAsync<IdentityRole>($"{ApiConstants.SingleRole}?roleId={roleId}");
-                return View(result);
-            }
-            catch (Exception ex)
-            {
-                DisplayErrors(ex);
-                return View();
-            }
-        }
+		[HttpGet]
+		public async Task<IActionResult> RolesList()
+		{
+			try
+			{
+				var result = await _client.GetAllAsync<IdentityRole>(ApiConstants.GetAllRoles);
+				return View(result);
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
 
-        [HttpGet]
-        public async Task<IActionResult> CreateRole()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateRole(Result<RoleRequestDTO> model)
-        {
-            try
-            {
-                var result = await _client.PostAsync<SuccessResultDTO>(ApiConstants.GetAllRoles, model.Response);
-                return RedirectToAction(nameof(RolesList));
-            }
-            catch (Exception ex)
-            {
-                DisplayErrors(ex);
-                return View();
-            }
-        }
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> EditRole(string roleId)
-        {
-            try
-            {
-                var result = await _client.GetAsync<RoleRequestDTO>($"{ApiConstants.SingleRole}?roleId={roleId}");
-                return View(result);
-            }
-            catch (Exception ex)
-            {
-                DisplayErrors(ex);
-                return View();
-            }
-        }
-        [HttpPost]
-        public async Task<IActionResult> EditRole(Result<RoleRequestDTO> model)
-        {
-            try
-            {
-                var result = await _client.PutAsync<SuccessResultDTO>(ApiConstants.SingleRole, model.Response);
+		[HttpGet]
+		public async Task<IActionResult> SingleRole(string roleId)
+		{
+			try
+			{
+				var result = await _client.GetAsync<IdentityRole>($"{ApiConstants.SingleRole}?roleId={roleId}");
+				return View(result);
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
+		}
 
-                if (result.Response.Succeeded)
-                    _toastService.AddSuccessToastMessage(result.Response.Message);
+		[HttpGet]
+		public async Task<IActionResult> CreateRole()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateRole(Result<RoleRequestDTO> model)
+		{
+			try
+			{
+				var result = await _client.PostAsync<SuccessResultDTO>(ApiConstants.GetAllRoles, model.Response);
+				return RedirectToAction(nameof(RolesList));
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
+		}
 
-                return RedirectToAction(nameof(RolesList));
-            }
-            catch(Exception ex) {
-                DisplayErrors(ex);
-                return View();
-            }
-        }
+		[HttpGet]
+		public async Task<IActionResult> EditRole(string roleId)
+		{
+			try
+			{
+				var result = await _client.GetAsync<RoleRequestDTO>($"{ApiConstants.SingleRole}?roleId={roleId}");
+				return View(result);
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
+		}
+		[HttpPost]
+		public async Task<IActionResult> EditRole(Result<RoleRequestDTO> model)
+		{
+			try
+			{
+				var result = await _client.PutAsync<SuccessResultDTO>(ApiConstants.SingleRole, model.Response);
 
-        [HttpGet]
-        public async Task<IActionResult> AddRemoveRoles(string userId)
-        {
-            try
-            {
-                var result = await _client.GetAsync<AddRemoveRoles>($"{ApiConstants.UserRoles}?userId={userId}");
-                return View(result);
-            }
-            catch (Exception ex)
-            {
-                DisplayErrors(ex);
-                return View();
-            }
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddRemoveRoles(Result<AddRemoveRoles> model)
-        {
-            try
-            {
-                var result = await _client.PostAsync<SuccessResultDTO>(ApiConstants.UserRoles, model.Response);
-                if (result.Response.Succeeded)
-                    _toastService.AddSuccessToastMessage(result.Response.Message);
+				if (result.Response.Succeeded)
+					_toastService.AddSuccessToastMessage(result.Response.Message);
 
-                return RedirectToAction("UsersList", "Account");
-            }
-            catch (Exception ex)
-            {
-                DisplayErrors(ex);
-                return View();
-            }
-        }
-    }
+				return RedirectToAction(nameof(RolesList));
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> AddRemoveRoles(string userId)
+		{
+			try
+			{
+				var result = await _client.GetAsync<AddRemoveRoles>($"{ApiConstants.UserRoles}?userId={userId}");
+				return View(result);
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddRemoveRoles(Result<AddRemoveRoles> model)
+		{
+			try
+			{
+				var result = await _client.PostAsync<SuccessResultDTO>(ApiConstants.UserRoles, model.Response);
+				if (result.Response.Succeeded)
+					_toastService.AddSuccessToastMessage(result.Response.Message);
+
+				return RedirectToAction("UsersList", "Account");
+			}
+			catch (Exception ex)
+			{
+				DisplayErrors(ex);
+				return View();
+			}
+		}
+	}
 }
