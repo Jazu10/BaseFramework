53ec54f9e62df2793ef21444435d6a999b914994
Ambareeshvs <ambareeshvs@gmail.com>
2023-03-27 11:41:52 +0530

    News Ad Added

--- Frontend/Frontend/Core/HttpClients/IGenericHttpClient.cs
+++ Frontend/Frontend/Core/HttpClients/IGenericHttpClient.cs
@@ -6,6 +6,7 @@ namespace Frontend.Core.HttpClients
     {
         Task<Result<List<TResponse>>> GetAllAsync<TResponse>(string address);
         Task<Result<List<TResponse>>> GetAsync<TResponse>(string address);
+        Task<Result<TResponse>> GetAsyncSingle<TResponse>(string address);
         Task<Result<TResponse>> PostAsync<TResponse>(string address, dynamic dynamicObject);
         Task<Result<TResponse>> PutAsync<TResponse>(string address, dynamic dynamicObject);
         Task<Result<TResponse>> DeleteAsync<TResponse>(string address);
