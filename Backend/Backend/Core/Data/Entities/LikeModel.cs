48fa2ea8751c31d6d40829742a6cfc9932b475b0
Ambareeshvs <ambareeshvs@gmail.com>
2023-03-27 11:43:21 +0530

    Like model pulled

---
+++ Backend/Backend/Core/Data/Entities/LikeModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Core.Data.Entities
+{
+    [Table("Likes")]
+    public class LikeModel
+    {
+        [ForeignKey("AspNetUsers")]
+        public string UserId { get; set; }
+        public string ContentId { get; set; }
+    }
+}
