f3ec15c2d073792bf766478f2c435661ebd1cb43
Jaseem <jaseemks11@gmail.com>
2023-03-27 11:00:30 +0530

    PostApproval

---
+++ Frontend/Frontend.DTO/Request/Advertisement/AdvertisementRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frontend.DTO.Response.Advertisement;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
+
+namespace Frontend.DTO.Request.Advertisement
+{
+    public class AdvertisementRequestDTO : AdvertisementResponseDTO
+    {
+        [JsonIgnore]
+        public List<IFormFile>? ImgFiles { get; set; }
+
+    }
+}
